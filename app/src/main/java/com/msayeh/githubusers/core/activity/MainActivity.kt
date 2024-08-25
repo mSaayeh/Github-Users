5233ec478ed3dbfd547d82b62bfe84be8355a6a8
Mohannad El-Sayeh <eng.mohannadelsayeh@gmail.com>
2024-08-25 16:32:05 +0300

    Setup Navigation

--- app/src/main/java/com/msayeh/githubusers/core/activity/MainActivity.kt
+++ app/src/main/java/com/msayeh/githubusers/core/activity/MainActivity.kt
@@ -11,8 +11,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
 import com.msayeh.githubusers.core.theme.GithubUsersTheme
+import com.msayeh.githubusers.features.profile.presentation.ProfileScreen
+import com.msayeh.githubusers.features.search.presentation.SearchScreen
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalSharedTransitionApi::class)
@@ -29,8 +33,14 @@ class MainActivity : ComponentActivity() {
                     ) {
                         val navController = rememberNavController()
 
-                        NavHost(navController = navController, startDestination = "") {
-
+                        NavHost(navController = navController, startDestination = Route.Search.route) {
+                            composable(Route.Search.route) {
+                                SearchScreen(navController)
+                            }
+                            composable(Route.Profile.route) { backStackEntry ->
+                                val username = Route.Profile.fromRoute(backStackEntry.toRoute())
+                                ProfileScreen(navController, username)
+                            }
                         }
                     }
                 }
